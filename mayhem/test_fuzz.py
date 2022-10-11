4fc12f6ca68f63b82aa3e00e49ad5d511af4850f
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-11 12:55:23 -0400

    attempt fix

--- mayhem/test_fuzz.py
+++ mayhem/test_fuzz.py
@@ -5,13 +5,18 @@ import sys
 with atheris.instrument_imports():
     import uncurl
 
-def testOneInput(data):
+def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
-    in_str = fdp.ConsumeUnicodeNoSurrogates(64)
-    uncurl.parse(in_str)
+    in_str = fdp.ConsumeUnicodeNoSurrogates(2048)
+
+    try:
+        uncurl.parse(in_str)
+    except:
+        pass
+    
         
 def main():
-    atheris.Setup(sys.argv, testOneInput)
+    atheris.Setup(sys.argv, TestOneInput)
     atheris.Fuzz()
 
 if __name__ == "__main__":
